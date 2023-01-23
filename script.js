fa6fb07953687804edfa49a287860af27788a705
Mahdis8160 <mahdis8160@gmail.com>
2023-01-23 13:33:42 -0800

    final result

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
- 
+var clear = false;
+var num1;
+var amalgar;
+
+
+
+
+
+
+
+function NumberClicked(number) {
+    var res = document.getElementById("result");
+    if(clear == true) {
+        num1 = res.value;
+        res.value = "";
+    }
+    res.value = res.value + number;
+
+
+    
+
+}
+function OprClicked(Opr) {
+    clear = true;
+    amalgar = Opr;
+}
+function EqualClicked() {
+    var res = document.getElementById("result");
+    var num2 = res.value;
+    switch(amalgar) {
+        case "+": 
+        res.value = parseInt(num1) + parseInt(num2);
+        break;
+        case "-":
+            res.value = parseInt(num1) - parseInt(num2);
+            break;
+            case "*":
+                res.value = parseInt(num1) * parseInt(num2);
+                break;
+                case "/":
+                    res.value = parseInt(num1) / parseInt(num2);
+                    break;
+                    
+                
+    }
+    
+}
+function ClearClicked() {
+    var res = document.getElementById("result");
+    res.value = "";
+    clear = false;
+    num1 = "";
+    num2 = "";
+    
+}
